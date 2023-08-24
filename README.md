36e944d88081b8983ae46e32857019c3c0903574
Chris <github@prettycode.org>
2023-08-24 07:46:03 -0700

    Misc. cleanup (e.g. README, linting, code comments, etc.)

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# npm-license-checker
+# Package Dependency License Checker
+
+This tool checks the licenses of dependencies in package.json files and outputs the results to the console and a CSV file.
+
+This tool was created to answer this question: out of ~20 node projects, do any have licenses that would prohibit commercial use?
+
+## How to Use
+
+1. **Copy package.jsons**: Copy any number of package.json files into the 'packageJsons' folder. Rename the files to whatever you want but keep the .json extension; all .json files in 'packageJsons' folder will be read by this tool.
+2. **Run the Script**: Execute the script via `npm start`.
+3. **Review the Results**: The console will display progress and results, and a CSV file will be created in the current directory. Open the CSV in a spreadsheet program and filter by license column.
+
+## Notes
+
+1. This tool was written in a couple of hours, i.e. is a quick/dirty solution.
+2. Ideally, this program would be improved to take a list of a git repositories (i.e. URLs of) and go fetch the package.json for each, vs. reading local files copied to `packageJsons`. Reason this was not done: the projects I needed to examine were private repositories, so it was quicker to copy the files vs. figure out the PAT token code required for fetching from the private repos.
--- licenseChecker.ts
+++ licenseChecker.ts
@@ -3,25 +3,49 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as semver from 'semver';
 
-function sleep(ms: number): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, ms));
+type PackageDotJsonDependencyType = 'dependencies' | 'devDependencies' | 'peerDependencies' | 'optionalDependencies';
+
+type PackageDotJsonDependency = {
+    projectName: string;
+    packageName: string;
+    packageVersion: string;
+    packageLicense?: string;
+};
+
+/**
+ * Pause execution asynchronously for `ms` milliseconds. Explanation is ignored; serves as a (forced) code comment.
+ */
+function sleep(explanation: string, ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+/**
+ * Move the cursor bax `charCount` characters.
+ */
 function backupConsole(charCount: number): string {
     return `\u001b[${charCount}D`;
 }
 
-function toCSV(data: Array<PackageDotJsonDependency>): string {
+/**
+ * Convert an array of objects to a CSV string. Does not escape values.
+ */
+function toCsv(data: Array<PackageDotJsonDependency>): string {
     const header = Object.keys(data[0]).join();
-    const rows = data.map(obj => Object.values(obj).join()).join('\n');
+    const rows = data.map((obj) => Object.values(obj).join()).join('\n');
 
     return `${header}\n${rows}`;
 }
 
+/**
+ * Return a timestamp string that is safe to use in a filename (no colons).
+ */
 function filesafeTimestamp(): string {
     return new Date().toISOString().replace(/:/g, '.');
 }
 
+/**
+ * Fetch the license for a package from the npmjs.org registry. Cache results if the lookup was successful.
+ */
 async function fetchLicenseForPackage(packageName: string, packageVersion: string): Promise<string> {
     const cacheLocation = fetchLicenseForPackage.prototype;
     let cache: Record<string, string> = cacheLocation.cache;
@@ -30,47 +54,46 @@ async function fetchLicenseForPackage(packageName: string, packageVersion: strin
         cache = cacheLocation.cache = {} as Record<string, string>;
     }
 
-    if (cache.hasOwnProperty(packageName)) {
-        return cacheLocation.cache[packageName];
+    const cacheKey = `${packageName}@${packageVersion}`;
+
+    if (cache[cacheKey]) {
+        return cacheLocation.cache[cacheKey];
     }
 
-    await sleep(1001);
-
     let packageLicense: string;
-    
+
+    if (packageVersion.toLowerCase().startsWith('file:') || packageVersion.toLowerCase().startsWith('link:')) {
+        return (cache[cacheKey] = packageLicense = `[SKIPPED: ${packageVersion}]`);
+    }
+
+    await sleep("Let's not DoS the npmjs.org registry", 1001);
+
+    const registryUrl = `https://registry.npmjs.org/${packageName}/${packageVersion}`;
+
     try {
-        const fetchResponse = await axios.get<Required<{ license: string }>>(
-            `https://registry.npmjs.org/${packageName}/${packageVersion}`
-        );
-    
-        cache[packageName] = packageLicense = fetchResponse.data.license || '[BLANK]';
+        const fetchResponse = await axios.get<Required<{ license: string }>>(registryUrl);
+        cache[cacheKey] = packageLicense = fetchResponse.data.license || '[BLANK]';
     } catch (error) {
         const axiosError = error as AxiosError;
-
         packageLicense = `[ERROR: ${axiosError.response?.status}: ${axiosError.response?.statusText}]`;
     }
 
     return packageLicense;
 }
 
-type PackageDotJsonDependencyType = 'dependencies' | 'devDependencies';
-
-type PackageDotJsonDependency = {
-    projectName: string;
-    packageName: string;
-    packageVersion: string;
-    packageLicense?: string;
-}
-
+/**
+ * There're different patterns for versio in package.json; try to get one we can reduce to "x.y.z".
+ */
 function getCleanNpmPackageVersion(packageJsonVersion: string): string {
-    return semver.valid(packageJsonVersion) || 
-        semver.valid(semver.coerce(packageJsonVersion)) || 
-        `[ERROR: UNHANDLED: ${packageJsonVersion}]`;
+    return semver.valid(packageJsonVersion) || semver.valid(semver.coerce(packageJsonVersion)) || packageJsonVersion;
 }
 
+/**
+ * Get dependencies from all *.json files in the `packageJsons` directory, of type `type`, in one flat array.
+ */
 async function getDependencies(type: PackageDotJsonDependencyType): Promise<Array<PackageDotJsonDependency>> {
     const packageFilesDir = 'packageJsons';
-    const filenames = (await fs.promises.readdir(packageFilesDir)).filter(fn => fn.endsWith('.json'));
+    const filenames = (await fs.promises.readdir(packageFilesDir)).filter((fn) => fn.endsWith('.json'));
     const allDependencies: Array<PackageDotJsonDependency> = [];
 
     for (const filename of filenames) {
@@ -85,7 +108,7 @@ async function getDependencies(type: PackageDotJsonDependencyType): Promise<Arra
         }
 
         for (const [packageName, version] of Object.entries(packageDependencies)) {
-            const project = filename.replace(path.extname(filename), ''); 
+            const project = filename.replace(path.extname(filename), '');
 
             allDependencies.push({
                 projectName: project,
@@ -98,8 +121,21 @@ async function getDependencies(type: PackageDotJsonDependencyType): Promise<Arra
     return allDependencies;
 }
 
+/**
+ * Output the table data of packages and their licenses to a CSV file.
+ */
+function writeCsvFile(packages: Array<PackageDotJsonDependency>, type: PackageDotJsonDependencyType): void {
+    const csvContent = toCsv(packages);
+    const csvFilename = `.${type}.${filesafeTimestamp()}.csv`;
+
+    fs.writeFileSync(csvFilename, csvContent);
+}
+
+/**
+ * Main function: check dependencies licenses and output the results to the console and a CSV file.
+ */
 async function checkDependencies(type: PackageDotJsonDependencyType): Promise<Array<PackageDotJsonDependency>> {
-    const consoleStream = process.stdout;    
+    const consoleStream = process.stdout;
     const packages = await getDependencies(type);
 
     for (const pkg of packages) {
@@ -107,17 +143,28 @@ async function checkDependencies(type: PackageDotJsonDependencyType): Promise<Ar
         const checkingDisplayMessage = `☐  Checking ${projectName}'s "${type}": ${packageName}@${packageVersion}... `;
 
         consoleStream.write(checkingDisplayMessage);
+
         pkg.packageLicense = await fetchLicenseForPackage(packageName, packageVersion);
+
+        if (pkg.packageLicense !== null && typeof pkg.packageLicense === 'object') {
+            const type = (pkg.packageLicense as Record<string, string>).type;
+
+            if (typeof type === 'string') {
+                pkg.packageLicense = type;
+            } else {
+                throw new Error('Unrecognized package license definition.');
+            }
+        }
+
         consoleStream.write(backupConsole(checkingDisplayMessage.length));
-        consoleStream.write(`${checkingDisplayMessage.replace('☐', pkg.packageLicense.startsWith('[') ? '✖' : '✔')}${pkg.packageLicense}.\n`);
+        consoleStream.write(
+            `${checkingDisplayMessage.replace('☐', pkg.packageLicense.startsWith('[') ? '✖' : '✔')}${pkg.packageLicense}.\n`
+        );
     }
 
     console.table(packages);
-    
-    const csvContent = toCSV(packages);
-    const csvFilename = `.${type}.${filesafeTimestamp()}.csv`;
 
-    fs.writeFileSync(csvFilename, csvContent);
+    writeCsvFile(packages, type);
 
     return packages;
 }
